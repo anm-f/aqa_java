cfc6b6869f2919fd5491f5c98066efe54a48c09c
Anton Marko <anton_marko@outlook.com>
2026-08-21 08:13:40 +0300

    adjusted lesson_7_junit

--- aqa_java_project/src/main/java/lesson2_7_junit_5/MathUtils.java
+++ aqa_java_project/src/main/java/lesson2_7_junit_5/MathUtils.java
@@ -27,8 +27,11 @@ public class MathUtils {
 
     // 3. Арифметические операции
     public int add(int a, int b) { return a + b; }
+
     public int subtract(int a, int b) { return a - b; }
+
     public int multiply(int a, int b) { return a * b; }
+
     public double divide(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Деление на ноль запрещено");
